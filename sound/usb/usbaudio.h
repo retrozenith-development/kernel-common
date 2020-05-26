4c9138d11e6d95ca9830be2ee83c14f66c6d613a
JaeHun Jung <jh0801.jung@samsung.com>
2020-05-26 16:53:38 +0900

    ANDROID: sound: usb: Add vendor's hooking interface
    In mobile, a co-processor can be used with USB audio to improve power
    consumption.  To support this type of hardware, hooks need to be added
    to the USB audio subsystem to be able to call into the hardware when
    needed.
    
    This interface can be support multiful USB audio devices.
    It is depends on co-processor's F/W.
    
    The main operation of the call-backs are:
    - Initialize the co-processor by transmitting data when initializing.
    - Change the co-processor setting value through the interface
    function.
    - Configure sampling rate
    - pcm open/close
    - other housekeeping
    
    Bug: 156315379
    Signed-off-by: Oh Eomji <eomji.oh@samsung.com>
    Signed-off-by: JaeHun Jung <jh0801.jung@samsung.com>
    [rework api to be a bit more self-contained and obvious - gregkh]
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I39061f6cc85be7bcae8db0e612fe58396bdedb24

--- sound/usb/usbaudio.h
+++ sound/usb/usbaudio.h
@@ -20,6 +20,7 @@
 
 struct media_device;
 struct media_intf_devnode;
+struct snd_usb_substream;
 
 #define MAX_CARD_INTERFACES	16
 
@@ -201,4 +202,41 @@ extern bool snd_usb_skip_validation;
 #define QUIRK_FLAG_SKIP_IMPLICIT_FB	(1U << 18)
 #define QUIRK_FLAG_IFACE_SKIP_CLOSE	(1U << 19)
 
+struct audioformat;
+
+enum snd_vendor_pcm_open_close {
+	SOUND_PCM_CLOSE = 0,
+	SOUND_PCM_OPEN,
+};
+
+/**
+ * struct snd_usb_audio_vendor_ops - function callbacks for USB audio accelerators
+ * @connect: called when a new interface is found
+ * @disconnect: called when an interface is removed
+ * @set_interface: called when an interface is initialized
+ * @set_pcm_intf: called when the pcm interface is set
+ * @set_pcm_connection: called when pcm is opened/closed
+ *
+ * Set of callbacks for some accelerated USB audio streaming hardware.
+ *
+ * TODO: make this USB host-controller specific, right now this only works for
+ * one USB controller in the system at a time, which is only realistic for
+ * self-contained systems like phones.
+ */
+struct snd_usb_audio_vendor_ops {
+	int (*connect)(struct usb_interface *intf);
+	void (*disconnect)(struct usb_interface *intf);
+	int (*set_interface)(struct usb_device *udev,
+			     struct usb_host_interface *alts,
+			     int iface, int alt);
+	int (*set_pcm_intf)(struct usb_interface *intf, int iface, int alt,
+			    int direction, struct snd_usb_substream *subs);
+	int (*set_pcm_connection)(struct usb_device *udev,
+				  enum snd_vendor_pcm_open_close onoff,
+				  int direction);
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
+	ANDROID_KABI_RESERVE(4);
+};
 #endif /* __USBAUDIO_H */
